2400a1923de5bf3a34b9209057476478851b6b88
Hexx King <hexxking13@gmail.com>
2022-07-16 16:30:10 -0700

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './Header';
+import Footer from './Footer';
+import Snack from './Snack';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Snack title={"Pretzels"} description={"Salty and Crunchy!"} />
+        <Snack title={"Oreos"} description={"Chocolately, crunchy with a lovely cream"}/>
+        <Snack title={"Cherry Tomatoes"} description={"juicy and Delicious"}/>
+        <Snack title={"Pickles"} description={"Salty and Crunchy!"} />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
